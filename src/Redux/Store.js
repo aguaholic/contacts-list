67cb224d092f54fef4696b95ec230daab23341de
aguaholic <joyterra@gmail.com>
2019-12-10 21:36:20 +0100

    Add redux thunk

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,16 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 import { contacts } from './Reducers/contacts'
 
+const enhance = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  thunk
+)
+
 const store = createStore(
   contacts,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  enhance
 )
 
 export default store
