645b4222a5b7ad9be5525cf11dddb186994f6a7d
aguaholic <joyterra@gmail.com>
2019-12-15 18:55:06 +0100

    Add styling to Card elements

--- src/Contacts/Card/Card.jsx
+++ src/Contacts/Card/Card.jsx
@@ -1,42 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 import classes from './Card.module.css'
-
 const Card = ({ contact, onClose }) => {
   return (
     <>
-      <div
+      <AiOutlineCloseCircle
         className={classes.Close}
-        onClick={onClose}>
-          X
-      </div>
+        onClick={onClose} />
       <div className={classes.Card}>
-        <div>
+        <div className={classes.Img}>
           <img src={contact.picture.medium} alt={contact.name.last} />
         </div>
-        <h2>{contact.name.last.toUpperCase()}, {contact.name.first}</h2>
-        <div>
-          <strong>email:</strong> {contact.email}
-        </div>
-        <div>
-          <strong>phone:</strong> {contact.phone}
-        </div>
-        <div>
-          <strong>street:</strong> {contact.location.street.name} {contact.location.street.number}
-        </div>
-        <div>
-          <strong>city:</strong> {contact.location.city}
-        </div>
-        <div>
-          <strong>state:</strong> {contact.location.state}
-        </div>
-        <div>
-          <strong>postcode:</strong> {contact.location.postcode}
+        <div className={classes.Text}>
+          <h2>{contact.name.last.toUpperCase()}, {contact.name.first}</h2>
+          <div>
+            <strong>email:</strong> {contact.email}
+          </div>
+          <div>
+            <strong>phone:</strong> {contact.phone}
+          </div>
+          <div>
+            <strong>street:</strong> {contact.location.street.name} {contact.location.street.number}
+          </div>
+          <div>
+            <strong>city:</strong> {contact.location.city}
+          </div>
+          <div>
+            <strong>state:</strong> {contact.location.state}
+          </div>
+          <div>
+            <strong>postcode:</strong> {contact.location.postcode}
+          </div>
         </div>
       </div>
     </>
-
   )
 }
 
