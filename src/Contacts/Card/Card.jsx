11f22025f3ec0db65625e67d7093ccdc687bcfc7
aguaholic <joyterra@gmail.com>
2019-12-15 16:48:36 +0100

    Add styling to card

--- src/Contacts/Card/Card.jsx
+++ src/Contacts/Card/Card.jsx
@@ -5,33 +5,38 @@ import classes from './Card.module.css'
 
 const Card = ({ contact, onClose }) => {
   return (
-    <div>
+    <>
       <div
         className={classes.Close}
         onClick={onClose}>
           X
       </div>
-      <h2>{contact.name.last.toUpperCase()}, {contact.name.first}</h2>
-      <div>
-        <strong>email:</strong> {contact.email}
+      <div className={classes.Card}>
+        <div>
+          <img src={contact.picture.medium} alt={contact.name.last} />
+        </div>
+        <h2>{contact.name.last.toUpperCase()}, {contact.name.first}</h2>
+        <div>
+          <strong>email:</strong> {contact.email}
+        </div>
+        <div>
+          <strong>phone:</strong> {contact.phone}
+        </div>
+        <div>
+          <strong>street:</strong> {contact.location.street.name} {contact.location.street.number}
+        </div>
+        <div>
+          <strong>city:</strong> {contact.location.city}
+        </div>
+        <div>
+          <strong>state:</strong> {contact.location.state}
+        </div>
+        <div>
+          <strong>postcode:</strong> {contact.location.postcode}
+        </div>
       </div>
-      <div>
-        <strong>phone:</strong> {contact.phone}
-      </div>
-      <div>
-        <strong>street:</strong> {contact.location.street.name} {contact.location.street.number}
-      </div>
-      <div>
-        <strong>city:</strong> {contact.location.city}
-      </div>
-      <div>
-        <strong>state:</strong> {contact.location.state}
-      </div>
-      <div>
-        <strong>postcode:</strong> {contact.location.postcode}
-      </div>
+    </>
 
-    </div>
   )
 }
 
